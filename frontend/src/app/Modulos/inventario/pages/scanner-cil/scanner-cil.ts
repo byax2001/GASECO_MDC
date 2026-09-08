c5dc4b2dd19426153f57be09340db867fbed5b35
MoonGSK <3043441920114@ingenieria.usac.edu.gt>
2026-09-08 12:53:24 -0600

    [ADD] Diseño y apartados extras Escaneo de Cilindros
    Se agregaron apartados extras, bloqueos y diseño para el escaneo de cilindros

--- frontend/src/app/Modulos/inventario/pages/scanner-cil/scanner-cil.ts
+++ frontend/src/app/Modulos/inventario/pages/scanner-cil/scanner-cil.ts
@@ -1,27 +1,36 @@
-import { Component, ChangeDetectionStrategy, signal, effect, inject, HostListener, ViewChild } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal, effect, inject, HostListener, ViewChild, computed } from '@angular/core';
 import { HeaderPage } from "../../../../shared/components/header-page/header-page";
-import { CilindroScan } from '../../components/CilindroScan.interface';
+import { CilindroScan } from '../../interfaces/CilindroScan.interface';
 import { EscaneoService } from './services/escaneo.service';
 import { Modalg } from "../../../../shared/components/modalg/modalg";
 import { TablaCilindrosEsc } from "./components/tabla-cilindros-esc/tabla-cilindros-esc";
 import { FilesAdmin } from '../../../../services/files-admin.service';
 import { ButtonIcon } from '../../../../shared/components/button-icon/button-icon';
 import { Modalact } from "../../../../shared/components/modalact/modalact";
+import { ModalScanCil } from "./components/modal-scan-cil/modal-scan-cil";
 
 @Component({
   selector: 'app-scanner-cil',
-  imports: [HeaderPage, Modalg, TablaCilindrosEsc, ButtonIcon, Modalact],
+  imports: [HeaderPage, Modalg, TablaCilindrosEsc, ButtonIcon, Modalact, ModalScanCil],
   templateUrl: './scanner-cil.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './scanner-cil.css',
 })
 export default class ScannerCil {
   cilindro = signal<string>('');
+  //Cilindros Escaneados
   cilindrosEscaneados = signal<CilindroScan[]>([]);
+  //Cilindros Temporales, mientras se designa el estado y si tienen etiqueta
+  cilindrosPendientes = signal<CilindroScan[]>([]);
+  //Marca si el cilindro siguiente no tiene etiqueta
+  sinEtiqueta = signal<boolean>(false)
+
   adminFileService = inject(FilesAdmin);
   escaneoService = inject(EscaneoService);
   @ViewChild('modalG') modalG!: Modalg;
   @ViewChild('modalAct') modalAct!: Modalact;
+  @ViewChild('modalScan') modalScan!: ModalScanCil;
+
   ngOnInit() {
     const storedCilindros = localStorage.getItem('cilindrosEscaneados');
     if (storedCilindros) {
@@ -29,15 +38,23 @@ export default class ScannerCil {
     }
   }
 
+  cilVacios = computed(()=>{
+    if(this.cilindrosEscaneados().length == 0) return 0
+    const cilVacios = this.cilindrosEscaneados().filter((cil => cil.ESTADOSCAN === "VAC"))
+    return cilVacios.length;
+  })
+
   debounceEffect = effect((onCleanup) =>{
     const value = this.cilindro();
     if(value === '') return; //Si el valor es vacío, no se emite nada
-
+    let tiempo = 1000;
+    //Si no tiene etiqueta se agrega mas tiempo para agregar a mano
+    if(this.sinEtiqueta()) tiempo+= 3000;
     const timeout = setTimeout(() => {
       //Luego de 500ms de inactividad, se emite el valor del input
       this.buscarCilindro(value);
       console.log('Emitted value:', value);
-    }, 2000);
+    }, tiempo);
 
     onCleanup(() => {
       clearTimeout(timeout);
@@ -56,29 +73,79 @@ export default class ScannerCil {
 
   //SE BUSCA EL CILINDRO EN BASE A SU SERIE
   buscarCilindro(cilindro?: string) {
-    console.log('Cilindro escaneado:', cilindro);
+    //Busca si hay un cilindro ya escaneado
+    const yaEscaneado = this.cilindrosEscaneados().some(
+      cil => cil.SERIE === cilindro
+    );
+
+    if(yaEscaneado){
+      this.modalG.showModalG('Error','Cilindro ya escaneado.');
+      this.cilindro.set('');
+      return;
+    }
+   
     this.escaneoService.buscarCilindro(cilindro!).subscribe({
       next: (data) => {
         if(data.length === 0){
+          this.cilindro.set('');
+          this.sinEtiqueta.set(false);
           this.modalG.showModalG('Cilindro no encontrado', 'No se encontro ningun cilindro con la serie: ' + cilindro + '.');
+          return;
         }
 
         if(data.length >1 ){
           this.modalG.showModalG('Cilindro Duplicado', 'Se encontro mas de un cilindro con la misma serie.');
         }
-        this.cilindrosEscaneados.update(actual => [
-          ...actual,
-          ...data
-        ]);
-        localStorage.setItem('cilindrosEscaneados', JSON.stringify(this.cilindrosEscaneados()));
+        
+
+        this.modalScan.showModalAct('Estado de Cilindro', 'Indique el Estado de Cilindro')
+         // Guardar temporalmente
+        this.cilindrosPendientes.set(data);
+
+        // Activa un Modal que asigna un Estado y el cual emite dependiendo de la opción
+        // Redirigiendo al metodo asignarEstado()
+        this.modalScan.showModalAct(
+          'Estado de Cilindro',
+          'Indique el Estado de Cilindro'
+        );
 
         this.cilindro.set('');
       },
       error: (error) => {
         console.error('Error al buscar el cilindro:', error);
+        this.sinEtiqueta.set(false)
         this.cilindro.set('');
       },
     });
+  }
+
+  asignarEstado(estado: string) {
+
+    if(estado!='CAN'){
+      //CAMBIA TODOS LOS CILINDROS AL ESTADO REQUERIDO (SIEMPRE SERIA UN UNICO CILINDRO)
+      const cilindrosConEstado = this.cilindrosPendientes().map(cil => ({
+        ...cil,
+        ESTADOSCAN: estado,
+        SINETIQUETA:this.sinEtiqueta()
+      }));
+      //SE ACTUALIZAL LOS CILINDROS ESCANEADOS
+      this.cilindrosEscaneados.update(actual => [
+        ...actual,
+        ...cilindrosConEstado
+      ]);
+
+      // SE ALMACENAN EN LOCAL STORAGE
+      localStorage.setItem(
+        'cilindrosEscaneados',
+        JSON.stringify(this.cilindrosEscaneados())
+      );
+    }
+    // SE VACIAN LOS CILINDROS PENDIENTES.
+    this.cilindrosPendientes.set([]);
+    this.sinEtiqueta.set(false);
+
+    // SE CIERRA EL MODAL
+    this.modalScan.closeModal();
   }
 
   removeLinea(index: number) {
@@ -92,17 +159,22 @@ export default class ScannerCil {
 
 
   descargarExcel() {
-    if(this.cilindrosEscaneados().length == 0){
+    if(this.cilindrosEscaneados().length === 0){
       alert('No hay datos para exportar');
       return;
     }
+
     const data = this.cilindrosEscaneados().map(cilindro => ({
       IDCILINDRO: cilindro.IDCILINDRO,
       SERIE: cilindro.SERIE,
       CVEPRODUCTO_CP: cilindro.CVEPRODUCTO_CP, 
       CVEENVASE: cilindro.CVEENVASE,
+      ESTADOSCANEO: cilindro.ESTADOSCAN,
+      SINETIQUETA: cilindro.SINETIQUETA,
       UBACTUAL: cilindro.UBACTUAL,
       REFUBACTUAL: cilindro.REFUBACTUAL,
+      FHULTMOV: new Date(cilindro.FHULTMOV),
+      DIASENCLIENTE: this.getDiasCliente(cilindro.UBACTUAL,cilindro.FHULTMOV),
       UBANTERIOR: cilindro.UBANTERIOR,
       REFUBANTERIOR: cilindro.REFUBANTERIOR,
       MTSCUBICOS: cilindro.MTSCUBICOS,
@@ -112,9 +184,6 @@ export default class ScannerCil {
       OBSERVACION: cilindro.OBSERVACION,
       NUMCERTIFICADO: cilindro.NUMCERTIFICADO,
       FECHAPH: cilindro.FECHAPH,
-      EXPPERMITIDA: cilindro.EXPPERMITIDA,
-      RANGOEXP: cilindro.RANGOEXP,
-      EXPTOTAL: cilindro.EXPTOTAL,
       PESO: cilindro.PESO,
       CAPACIDAD: cilindro.CAPACIDAD,
       FABRICANTE: cilindro.FABRICANTE,
@@ -124,7 +193,6 @@ export default class ScannerCil {
       SUCURSAL: cilindro.SUCURSAL,
       NORMA_FABRICACION: cilindro.NORMA_FABRICACION,
       COMPANIA: cilindro.COMPANIA,
-      FHULTMOV: cilindro.FHULTMOV,
       ULOTELLE: cilindro.ULOTELLE,
       FHULTLLE: cilindro.FHULTLLE,
       DESCCORTA: cilindro.DESCCORTA,
@@ -132,6 +200,18 @@ export default class ScannerCil {
       LINEA_CUBO: cilindro.LINEA_CUBO
     }));
     this.adminFileService.descargarXLSX(data, 'CilindrosEscaneados');
+  }
+
+  getDiasCliente(Ubacutal:string, fhultmov:Date):number{
+    if(Ubacutal!=="CLI") return 0;
+
+    const entrega = new Date(fhultmov);
+    entrega.setHours(0, 0, 0, 0);
+
+    return Math.floor(
+        (Date.now() - entrega.getTime()) /
+        (1000 * 60 * 60 * 24)
+      )
   }
 
   limpiarCilindros() {
--- frontend/src/app/Modulos/inventario/pages/scanner-cil/services/escaneo.service.ts
+++ frontend/src/app/Modulos/inventario/pages/scanner-cil/services/escaneo.service.ts
@@ -3,7 +3,7 @@ import { UserInfoService } from '../../../../../services/userInfo.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../../../environments/environment';
 import { Observable } from 'rxjs';
-import { CilindroScan } from '../../../components/CilindroScan.interface';
+import { CilindroScan } from '../../../interfaces/CilindroScan.interface';
 
 @Service()
 export class EscaneoService {
